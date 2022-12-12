51f44fb72389052b5e944ad3b5004e594885a5d7
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-12-12 06:50:23 +0100

    Add XUnit integration tests

---
+++ TestingWebApi.XUnitIntegrationTests/PizzasIntegrationTests.cs
@@ -0,0 +1,95 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TestingWebApi.Core;
+using TestingWebApi.Core.Models;
+
+namespace TestingWebApi.XUnitIntegrationTests;
+
+[Collection("IntegrationTests")]
+public sealed class PizzasIntegrationTests : IClassFixture<WebApplicationFactory<Startup>>, IDisposable
+{
+    private readonly HttpClient _httpClient;
+
+    public PizzasIntegrationTests(WebApplicationFactory<Startup> factory)
+    {
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Get_BasicCase_200WithAllPizzas()
+    {
+        // Given
+        HttpMethod httpMethod = HttpMethod.Get;
+        const string requestUri = "/pizzas";
+        var request = new HttpRequestMessage(httpMethod, requestUri);
+
+        // When
+        HttpResponseMessage response = await _httpClient.SendAsync(request);
+
+        // Then
+        response.EnsureSuccessStatusCode();
+        var pizzas = await response.Content.ReadFromJsonAsync<List<Pizza>>();
+        Assert.NotNull(pizzas);
+        Assert.NotEmpty(pizzas);
+    }
+
+    [Fact]
+    public async Task Get_WithIdExisting_200WithOnePizza()
+    {
+        // Given
+        HttpMethod httpMethod = HttpMethod.Get;
+        const int pizzaId = 6;
+        var requestUri = $"/pizzas/{pizzaId}";
+        var request = new HttpRequestMessage(httpMethod, requestUri);
+
+        var expectedPizza = new Pizza(pizzaId, "Seven Cheese", true);
+
+        // When
+        HttpResponseMessage response = await _httpClient.SendAsync(request);
+
+        // Then
+        response.EnsureSuccessStatusCode();
+        var pizza = await response.Content.ReadFromJsonAsync<Pizza>();
+        Assert.NotNull(pizza);
+        Assert.Equal(expectedPizza, pizza);
+    }
+
+    [Fact]
+    public async Task Get_WithIdNotExisting_404()
+    {
+        // Given
+        HttpMethod httpMethod = HttpMethod.Get;
+        const int pizzaId = 9999;
+        var requestUri = $"/pizzas/{pizzaId}";
+        var request = new HttpRequestMessage(httpMethod, requestUri);
+
+        // When
+        HttpResponseMessage response = await _httpClient.SendAsync(request);
+
+        // Then
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task DemonstrateAssertionError()
+    {
+        // Given
+        const int pizzaId = 6;
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/pizzas/{pizzaId}");
+
+        var expectedPizza = new Pizza(pizzaId, "One hundred Cheese", true);
+
+        // When
+        HttpResponseMessage response = await _httpClient.SendAsync(request);
+
+        // Then
+        var pizza = await response.Content.ReadFromJsonAsync<Pizza>();
+        Assert.Equal(expectedPizza, pizza);
+    }
+
+    public void Dispose()
+    {
+        _httpClient.Dispose();
+    }
+}
